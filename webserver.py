336609137a79ef628d4e01d2b839c33888320d1c
Daniel Duran Saavedra <163008248+danieladsa@users.noreply.github.com>
2024-09-11 19:32:49 -0300

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -2,10 +2,15 @@ from flask import Flask
 from threading import Thread
 
 app = Flask('')
+
 @app.route('/')
 def index():
     return "Your bot is alive!"
 
+def run():
+    # Asegúrate de que Flask escuche en 0.0.0.0
+    app.run(host="0.0.0.0", port=8080)
+
 def keep_alive():
-    server = Thread(target=app.run)
+    server = Thread(target=run)
     server.start()
